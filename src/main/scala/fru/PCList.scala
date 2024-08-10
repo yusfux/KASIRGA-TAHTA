4c18a905dfc0c23d92d608f8643a86220e200c29
akira <105945643+yusfux@users.noreply.github.com>
2024-08-10 15:03:26 +0300

    feat: add PCList (#86)

---
+++ src/main/scala/fru/PCList.scala
@@ -0,0 +1,43 @@
+package wood.fru
+
+import chisel3._
+import chisel3.util._
+import wood.WoodConfig
+
+class PCListIO(config: WoodConfig) extends Bundle {
+  val fetch1 = new Bundle {
+    val fetchpc = Flipped(DecoupledIO(UInt(config.pcWidth.W)))
+    val tag = Output(UInt(log2Ceil(config.pcListDepth).W))
+  }
+
+  val exu = new Bundle {
+    val tag = Input(UInt(log2Ceil(config.pcListDepth).W))
+    val pc = Output(UInt(config.pcWidth.W))
+    val retiretag = Input(Bool())
+  }
+}
+
+class PCList(config: WoodConfig) extends Module {
+  val io = IO(new PCListIO(config))
+
+  val depth = config.pcListDepth
+  val pclen = config.pcWidth
+  val validlen = 1
+
+  val pclist = RegInit(VecInit(Seq.fill(depth)(0.U((validlen + pclen).W))))
+
+  val isfull = pclist.map(x => x(pclen)).reduce(_ & _)
+  val freeIdx = pclist.indexWhere(x => x(pclen) === 0.U)
+
+  when(io.fetch1.fetchpc.fire) {
+    pclist(freeIdx) := Cat(1.U, io.fetch1.fetchpc.bits)
+  }
+
+  when(io.exu.retiretag) {
+    pclist(io.exu.tag) := 0.U
+  }
+
+  io.fetch1.fetchpc.ready := !isfull
+  io.fetch1.tag := freeIdx
+  io.exu.pc := pclist(io.exu.tag)(pclen - 1, 0)
+}
