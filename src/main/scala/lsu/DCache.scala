0b50870a7ced64463915d4c5a2fe30940af0fd23
akira <105945643+yusfux@users.noreply.github.com>
2024-08-27 11:23:46 +0300

    feat: add wstrb to DCache (#132)

--- src/main/scala/lsu/DCache.scala
+++ src/main/scala/lsu/DCache.scala
@@ -8,23 +8,24 @@ class DCache(config: WoodConfig) extends Module {
   val io = IO(new Bundle {
     val core = new Bundle {
       val req = Flipped(DecoupledIO(new Bundle {
-        val addr = UInt(32.W)
-        val data = UInt(config.ddatalen.W)
-        val wen  = Bool()
+        val addr  = UInt(32.W)
+        val data  = UInt(config.ddatalen.W)
+        val wstrb = Vec(config.ddatalen / 8, Bool())
+        val wen   = Bool()
       }))
       val resp = DecoupledIO(new Bundle {
-        val data = UInt(128.W)
+        val data = UInt(config.ddatalen.W)
       })
     }
 
     val mem = new Bundle {
       val req = DecoupledIO(new Bundle {
         val addr = UInt(32.W)
-        val data = UInt(config.ddatalen.W)
+        val data = UInt(config.mmInterfaceWidth.W)
         val wen  = Bool()
       })
       val resp = Flipped(DecoupledIO(new Bundle {
-        val data = UInt(128.W)
+        val data = UInt(config.mmInterfaceWidth.W)
       }))
     }
   })
@@ -58,14 +59,17 @@ class DCache(config: WoodConfig) extends Module {
 
   val addrReg  = RegEnable(io.core.req.bits.addr, 0.U, io.core.req.fire)
   val wenReg   = RegEnable(io.core.req.bits.wen, false.B, io.core.req.fire)
+  val wstrbReg = RegEnable(io.core.req.bits.wstrb, VecInit(Seq.fill(16)(false.B)), io.core.req.fire)
   val wdataReg = RegEnable(io.core.req.bits.data, 0.U, io.core.req.fire)
+  val tagReg   = RegEnable(io.core.req.bits.addr(32 - 1, log2Ceil(config.dcacheDepth) + log2Ceil(config.ddatalen / 8)), 0.U, io.core.req.fire)
   val idxReg = RegEnable(
     io.core.req.bits.addr(log2Ceil(config.dcacheDepth) + log2Ceil(config.ddatalen / 8) - 1, log2Ceil(config.ddatalen / 8)),
     0.U,
     io.core.req.fire
   )
-  val tagReg = RegEnable(io.core.req.bits.addr(32 - 1, log2Ceil(config.dcacheDepth) + log2Ceil(config.ddatalen / 8)), 0.U, io.core.req.fire)
+
   val idx    = io.core.req.bits.addr(log2Ceil(config.dcacheDepth) + log2Ceil(config.ddatalen / 8) - 1, log2Ceil(config.ddatalen / 8))
+  val wrfull = wstrbReg.reduce(_ && _)
 
   val dataList  = WireInit(VecInit(Seq.fill(4)(0.U(config.ddatalen.W))))
   val tagList   = WireInit(VecInit(Seq.fill(4)(0.U(config.dtaglen.W))))
@@ -92,8 +96,7 @@ class DCache(config: WoodConfig) extends Module {
     }
   }
 
-  val hitWay = OHToUInt(matchList)
-  //TODO: idk why but when i try to update with victimWay as index to update the plru tree, it doesn't work
+  val hitWay       = OHToUInt(matchList)
   val victimUpdate = Cat(~plru0(idxReg), Mux(plru0(idxReg) === 0.U, ~plru2(idxReg), ~plru1(idxReg)))
   val victimWay    = RegEnable(victimUpdate, 0.U, io.core.req.fire)
   val victimTag    = tagList(victimWay)
@@ -104,12 +107,22 @@ class DCache(config: WoodConfig) extends Module {
   val isDirty = dirtyList(victimWay)
   val isValid = validList(victimWay)
 
+  val memData = RegEnable(io.mem.resp.bits.data, 0.U, io.mem.resp.fire)
+
+  val updatedData = VecInit(Seq.tabulate(16) { i =>
+    Mux(wstrbReg(i), wdataReg(8 * i + 7, 8 * i), dataList(hitWay)(8 * i + 7, 8 * i))
+  })
+  val updatedMemData = VecInit(Seq.tabulate(16) { i =>
+    Mux(wstrbReg(i), wdataReg(8 * i + 7, 8 * i), io.mem.resp.bits.data(8 * i + 7, 8 * i))
+  })
+
   val cacheAddr        = Mux(io.core.req.fire, idx, idxReg)
   val cacheWriteHit    = (isAccess && isHit && isWrite)
   val cacheWriteVictim = (io.mem.req.fire && isWrite) || (io.mem.resp.fire && ~isWrite)
   val cacheWriteFirst  = (isAccess && ~isHit && ~isDirty && isWrite)
   val cacheRead        = io.core.req.fire
-  val cacheData        = Mux(io.mem.resp.fire && ~isWrite, io.mem.resp.bits.data, wdataReg)
+  val cacheData =
+    Mux(io.mem.resp.fire && ~isWrite, io.mem.resp.bits.data, Mux(io.mem.resp.fire && isWrite && isMemRead, updatedMemData.asUInt, updatedData.asUInt))
 
   val tagAddr        = Mux(io.core.req.fire, idx, idxReg)
   val tagWriteHit    = (isAccess && isHit && isWrite)
@@ -123,9 +136,7 @@ class DCache(config: WoodConfig) extends Module {
     dcachebankio(i).writeData := cacheData
     dcachebankio(i).enable    := cacheRead || (cacheWriteHit && i.U === hitWay) || ((cacheWriteVictim || cacheWriteFirst) && i.U === victimWay)
     dcachebankio(i).isWrite   := (cacheWriteHit && i.U === hitWay) || ((cacheWriteVictim || cacheWriteFirst) && i.U === victimWay)
-  }
 
-  (0 until 4).foreach { i =>
     tagbankio(i).address   := tagAddr
     tagbankio(i).writeData := tagData
     tagbankio(i).enable    := tagRead || (tagWriteHit && i.U === hitWay) || ((tagWriteVictim || tagWriteFirst) && i.U === victimWay)
@@ -167,21 +178,20 @@ class DCache(config: WoodConfig) extends Module {
       }.otherwise {
         updatePlruTree(idxReg, victimUpdate)
         when(isWrite) {
-          state := Mux(isDirty, memWrite, idle)
+          state := Mux(isDirty, memWrite, Mux(wrfull, idle, memRead))
         }.otherwise {
           state := Mux(isDirty, memWrite, memRead)
         }
       }
     }
     is(memWrite) {
-      //TODO: no need to wait the mem response signal for write operation,
       when(io.mem.req.fire) {
-        state := Mux(isWrite, idle, memRead)
+        state := Mux(isWrite && wrfull, idle, memRead)
       }
     }
     is(memRead) {
       when(io.mem.resp.fire) {
-        state := response
+        state := Mux(isWrite, idle, response)
       }
     }
     is(response) {
@@ -190,8 +200,6 @@ class DCache(config: WoodConfig) extends Module {
       }
     }
   }
-
-  val memData = RegEnable(io.mem.resp.bits.data, 0.U, io.mem.resp.fire)
 
   io.core.req.ready      := isIdle
   io.core.resp.bits.data := Mux(isHit, dataList(hitWay), memData)
--- src/test/scala/lsu/DCacheSpec.scala
+++ src/test/scala/lsu/DCacheSpec.scala
@@ -13,9 +13,10 @@ class DCacheDut(config: WoodConfig) extends Module {
   val io = IO(new Bundle {
     val core = new Bundle {
       val req = Flipped(DecoupledIO(new Bundle {
-        val addr = UInt(32.W)
-        val data = UInt(128.W)
-        val wen  = Bool()
+        val addr  = UInt(32.W)
+        val data  = UInt(128.W)
+        val wstrb = Vec(16, Bool())
+        val wen   = Bool()
       }))
       val resp = DecoupledIO(new Bundle {
         val data = UInt(128.W)
@@ -51,13 +52,14 @@ class DCacheSpec extends AnyFlatSpec with ChiselScalatestTester {
   val config = new WoodConfig
 
   val MEM_DEPTH = 4096
-  val TEST_SIZE = 32
+  val TEST_SIZE = 4096
 
   val bytes   = Array("0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "a", "b", "c", "d", "e", "f")
   val dataseq = ArrayBuffer.fill(MEM_DEPTH)(scala.util.Random.shuffle(bytes.toSeq).mkString + scala.util.Random.shuffle(bytes.toSeq).mkString)
 
   "DCache" should "work" in {
     test(new DCacheDut(config)).withAnnotations(GetBackendAnnotation()) { dut =>
+      dut.io.core.req.bits.wstrb.foreach(_.poke(true.B))
       for (i <- 0 until TEST_SIZE) {
         dut.io.core.req.valid.poke(true.B)
         dut.io.core.req.bits.addr.poke((i * 16).U)
@@ -91,6 +93,7 @@ class DCacheSpec extends AnyFlatSpec with ChiselScalatestTester {
 
   "DCache" should "work random" in {
     test(new DCacheDut(config)).withAnnotations(GetBackendAnnotation()) { dut =>
+      dut.io.core.req.bits.wstrb.foreach(_.poke(true.B))
       for (i <- 0 until MEM_DEPTH) {
         dut.io.core.req.valid.poke(true.B)
         dut.io.core.req.bits.addr.poke((i * 16).U)
@@ -111,7 +114,6 @@ class DCacheSpec extends AnyFlatSpec with ChiselScalatestTester {
       for (i <- 0 until TEST_SIZE) {
         val readcnt  = scala.util.Random.nextInt(10) + 1
         val writecnt = scala.util.Random.nextInt(10) + 1
-
         for (i <- 0 until readcnt) {
           val addr = scala.util.Random.nextInt(MEM_DEPTH)
           dut.io.core.req.valid.poke(true.B)
@@ -131,11 +133,13 @@ class DCacheSpec extends AnyFlatSpec with ChiselScalatestTester {
         }
 
         for (i <- 0 until writecnt) {
-          val addr = scala.util.Random.nextInt(MEM_DEPTH)
-          val data = scala.util.Random.shuffle(bytes.toSeq).mkString + scala.util.Random.shuffle(bytes.toSeq).mkString
-          dataseq(addr) = data
+          val wstrb = scala.util.Random.nextInt(1 << 16 - 1).toBinaryString.padTo(16, '0')
+          val addr  = scala.util.Random.nextInt(MEM_DEPTH)
+          val data  = scala.util.Random.shuffle(bytes.toSeq).mkString + scala.util.Random.shuffle(bytes.toSeq).mkString
+          dataseq(addr) = updateData(wstrb, data, dataseq(addr))
           dut.io.core.req.valid.poke(true.B)
           dut.io.core.req.bits.addr.poke((addr * 16).U)
+          for (i <- 0 until 16) dut.io.core.req.bits.wstrb(i).poke((wstrb(15 - i) == '1').B)
           dut.io.core.req.bits.data.poke(s"h${data}".U)
           dut.io.core.req.bits.wen.poke(true.B)
           while (!dut.io.core.req.ready.peekBoolean()) {
@@ -144,6 +148,18 @@ class DCacheSpec extends AnyFlatSpec with ChiselScalatestTester {
           step()
         }
       }
+    }
+
+    def updateData(wstrb: String, data0: String, data1: String): String = {
+      var updatedData = ""
+      for (i <- 0 until 16) {
+        if (wstrb(i) == '1')
+          updatedData += data0.substring(2 * i, 2 * i + 2)
+        else
+          updatedData += data1.substring(2 * i, 2 * i + 2)
+      }
+      println(s"wstrb: ${wstrb}, data: ${data0}, data: ${data1}, updatedData: ${updatedData}")
+      return updatedData
     }
   }
 
